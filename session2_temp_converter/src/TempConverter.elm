90ea457cf974915f4b2d0d058e2407070dd0e571
kongo <d.parshenko@gmail.com>
2019-03-06 18:35:27 +0200

    Convert temperature values in both directions

--- session2_temp_converter/src/TempConverter.elm
+++ session2_temp_converter/src/TempConverter.elm
@@ -7,29 +7,32 @@ import Html.Attributes exposing (..)
 
 
 type alias Model =
-    { text : String }
+    { c : String, f : String }
 
 initialModel =
-    { text = "" }
+    { c = "", f = ""}
 
 type Msg =
-    UpdateText String
+    CelsiusUpdated String
+      | FarenheitUpdated String
 
-celsiusToFarenheit c = toFloat(c) * 9 / 5 + 32
+celsiusToFarenheit c = c * 9 / 5 + 32
+farenheitToCelsius f = (f - 32) * (5/9)
 
-inputToOutput s =
-  case String.toInt s of
+inputToOutput converter s =
+  case String.toFloat s of
     Nothing ->
       ""
     Just a ->
-      a |> celsiusToFarenheit |> String.fromFloat
+      a |> converter |> String.fromFloat
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    UpdateText s ->
-      { model | text = inputToOutput s }
-
+    CelsiusUpdated s ->
+      { model | c = s, f = inputToOutput celsiusToFarenheit s }
+    FarenheitUpdated s ->
+      { model | f = s, c = inputToOutput farenheitToCelsius s }
 
 view : Model -> Html Msg
 view model =
@@ -37,14 +40,16 @@ view model =
         [ input
           [ type_ "text"
           , autocomplete False
-          , onInput UpdateText
+          , value model.c
+          , onInput CelsiusUpdated
           ]
           []
         , text " Celsius = "
         , input
           [ type_ "text"
           , autocomplete False
-          , value model.text
+          , value model.f
+          , onInput FarenheitUpdated
           ]
           []
         , text " Farenheit"
