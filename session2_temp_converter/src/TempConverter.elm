f3be498d4083c9138b27765acd2f3cac60013896
kongo <d.parshenko@gmail.com>
2019-03-07 02:11:26 +0200

    Code formatting

--- session2_temp_converter/src/TempConverter.elm
+++ session2_temp_converter/src/TempConverter.elm
@@ -1,78 +1,94 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, text, input)
-import Html.Events exposing (onInput)
+import Html exposing (Html, div, input, text)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 
 type alias Model =
     { c : Maybe Float, f : Maybe Float }
 
+
 initialModel =
     { c = Nothing, f = Nothing }
 
-type Msg =
-    CelsiusUpdated String
-      | FarenheitUpdated String
 
-celsiusToFarenheit c = c * 9 / 5 + 32
-farenheitToCelsius f = (f - 32) * (5/9)
+type Msg
+    = CelsiusUpdated String
+    | FarenheitUpdated String
+
+
+celsiusToFarenheit c =
+    c * 9 / 5 + 32
+
+
+farenheitToCelsius f =
+    (f - 32) * (5 / 9)
+
 
 convertInputData converter s =
-  case String.toFloat s of
-    Nothing ->
-      Nothing
-    Just a ->
-      Just (converter a)
+    case String.toFloat s of
+        Nothing ->
+            Nothing
+
+        Just a ->
+            Just (converter a)
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    CelsiusUpdated s ->
-      { model | c = String.toFloat(s), f = convertInputData celsiusToFarenheit s }
-    FarenheitUpdated s ->
-      { model | f = String.toFloat(s), c = convertInputData farenheitToCelsius s }
+    case msg of
+        CelsiusUpdated s ->
+            { model | c = String.toFloat s, f = convertInputData celsiusToFarenheit s }
+
+        FarenheitUpdated s ->
+            { model | f = String.toFloat s, c = convertInputData farenheitToCelsius s }
+
 
 decorateModel model =
     let
         decorateMaybeFloat x =
             case x of
-              Nothing -> ""
-              Just y -> String.fromFloat y
+                Nothing ->
+                    ""
+
+                Just y ->
+                    String.fromFloat y
     in
-        { c = decorateMaybeFloat model.c, f = decorateMaybeFloat model.f }
+    { c = decorateMaybeFloat model.c, f = decorateMaybeFloat model.f }
+
 
 view : Model -> Html Msg
 view model =
     let
-        decoratedModel = decorateModel model
+        decoratedModel =
+            decorateModel model
     in
-        div []
-            [ input
-              [ type_ "text"
-              , autocomplete False
-              , value decoratedModel.c
-              , onInput CelsiusUpdated
-              ]
-              []
-            , text " Celsius = "
-            , input
-              [ type_ "text"
-              , autocomplete False
-              , value decoratedModel.f
-              , onInput FarenheitUpdated
-              ]
-              []
-            , text " Farenheit"
+    div []
+        [ input
+            [ type_ "text"
+            , autocomplete False
+            , value decoratedModel.c
+            , onInput CelsiusUpdated
             ]
+            []
+        , text " Celsius = "
+        , input
+            [ type_ "text"
+            , autocomplete False
+            , value decoratedModel.f
+            , onInput FarenheitUpdated
+            ]
+            []
+        , text " Farenheit"
+        ]
 
 
 main : Program () Model Msg
 main =
-  Browser.sandbox
-  { init = initialModel
-  , view = view
-  , update = update
-  }
-
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
